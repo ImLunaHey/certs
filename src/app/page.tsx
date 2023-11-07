c194872f1d3414f86a1d6d5d2657360f8f273b8d
luna <luna@wvvw.me>
2023-11-07 17:43:20 +1030

    chore: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,75 @@
-import Image from 'next/image'
+'use client';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import useWebSocket from 'react-use-websocket';
+
+// For example a: 0, b: 100
+// This will produce a random number between 0 and 100
+// This includes both the starting and ending numbers
+const randomNumberBetween = (a: number, b: number) => {
+  return Math.floor(Math.random() * (b - a + 1) + a);
+};
+
+const formatNumber = (number: number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
 export default function Home() {
+  const [certsIssued, setCertsIssued] = useState(0);
+  const [domains, setDomains] = useState<{ domain: string; top: number; left: number }[]>([]);
+  const { lastMessage } = useWebSocket('wss://certstream.calidog.io');
+
+  useEffect(() => {
+    const handleNewDomain = (domain: string) => {
+      setCertsIssued((prevCertsIssued) => prevCertsIssued + 1);
+      // Only add 1/100th of the domains to the list
+      if (randomNumberBetween(1, 100) !== 1) {
+        return;
+      }
+      setDomains((prevDomains) => [
+        ...prevDomains,
+        {
+          domain,
+          top: randomNumberBetween(0, 100),
+          left: randomNumberBetween(0, 100),
+        },
+      ]);
+    };
+
+    try {
+      if (lastMessage !== null) {
+        handleNewDomain(JSON.parse(lastMessage.data).data.leaf_cert.all_domains[0]);
+      }
+    } catch {}
+  }, [lastMessage]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex h-[100dvh] w-[100dvw] text-white text-sm font-mono bg-black">
+      <span className="text-black font-mono text-sm absolute bg-white p-5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded">
+        Certs issued since page load: {formatNumber(certsIssued)}
+      </span>
+      {domains.map(({ domain, top, left }, index) => {
+        return (
+          <motion.div
+            key={`${domain}-${index}`}
+            className="absolute"
+            style={{
+              top: `${top}dvh`,
+              left: `${left}dvw`,
+            }}
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+            }}
+            transition={{ ease: 'linear', duration: 4, delay: 1 }}
+            onAnimationComplete={() => {
+              setDomains((prevDomains) => prevDomains.filter((d) => d.domain !== domain));
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            {domain}
+          </motion.div>
+        );
+      })}
     </main>
-  )
+  );
 }
